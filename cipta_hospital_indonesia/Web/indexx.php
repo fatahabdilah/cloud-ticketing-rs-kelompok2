a8ae7ac7f97a6e676ebb834077d81eeecdfddd85
Nash <162543715+Nashdax@users.noreply.github.com>
2025-05-26 17:53:23 +0700

    Add files via upload

---
+++ cipta_hospital_indonesia/Web/indexx.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+<!-- font -->
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <img src="Asset/logobgwhite.svg" alt="">
+        <ul>
+            <li><a href="">Beranda</a></li>
+            <li><a href="">Tentang</a></li>
+            <li><a href="">FAQ</a></li>
+        </ul>
+        <button>MASUK / DAFTAR</button>
+    </nav>
+    <section>
+        <div>
+            <img src="Asset/logobgblue.svg" alt="">
+        </div>
+        <div>
+            <p>Melayani dari Hati, Membangkitkan Harapan</p>
+        </div>
+    </section>
+    <section>
+        <div>
+            <h1></h1>
+            <p></p>
+        </div>
+    <div>
+        <img src="" alt="">
+    </div>
+    </section>
+    <section>
+        <div>
+            <h2></h2>
+            <p></p>
+
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </section>
+    <section>
+        <div>
+            <button></button>
+            <p></p>
+        </div>
+        <div>
+            <button></button>
+            <p></p>
+        </div>
+    </section>
+    <section>
+        <div>
+            <button></button>
+        </div>
+        <dialog>
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </dialog>
+    </section>
+    <section>
+        <div>
+            <h2></h2>
+            <p></p>
+        </div>
+
+        <div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+             <div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div>
+            <div>
+                <img src="" alt="">
+                <p></p>
+            </div>
+            <div>
+                <h2></h2>
+                <ul>
+                    <li><i></i><a href=""></a></li>
+                    <li><i></i><a href=""></a></li>
+                    <li><i></i><a href=""></a></li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <a href=""></a>
+        </div>
+    </footer>
+</body>
+</html>
